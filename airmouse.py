2d6f382733a6b7e41d05b55e2e82f7ac0775758d
BN <78746273+bathri77@users.noreply.github.com>
2023-12-18 05:23:59 +0530

    Update airmouse.py

--- airmouse.py
+++ airmouse.py
@@ -1,9 +1,3 @@
-# This Python code is an extension script for the Air Mouse using Arduino and MPU6050.
-# Visit the Github page for the Arduino code and other information related to the project.
-# https://github.com/rupava/Air-Mouse-Using-Arduino-and-MPU6050
-# -By Rupava Baruah
-
-
 import serial
 from serial import Serial
 from pynput.mouse import Button, Controller
@@ -20,13 +14,13 @@ try:
         if data[0] == "DATAL":  # Checking if the identifier is "DATAL" which the Arduino sends the data as the gyro X, Y and Z values
             mouse.move(int(data[1]),int(data[2]))  # Moving the mouse by using the X and Y values after converting them into integer
 
-        '''if data[0] == "DATAB":  # Checking if the identifier is "DATAB" which the Arduino sends the values for Left/Right button
+        if data[0] == "DATAB":  # Checking if the identifier is "DATAB" which the Arduino sends the values for Left/Right button
             if data[1] == 'L':  # If the Left button is pressed
                 #mouse.press(Button.left)  # The corresponding button is pressed and released
                 #mouse.release(Button.left)
             if data[1] == 'R':  # If the Right button is pressed
                 mouse.press(Button.right)  # The corresponding button is pressed and released
-                mouse.release(Button.right)'''
+                mouse.release(Button.right)
 except:
     print("Mouse not found or disconnected.")
     k = input("Press any key to exit.")
